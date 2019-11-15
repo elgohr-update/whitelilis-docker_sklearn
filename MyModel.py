e67f09856772ed7e1a23dd7a333aba18c7271f22
whitelilis <whitelilis@gmail.com>
2019-11-15 17:41:20 +0800

    support Gunicorn add load()

--- MyModel.py
+++ MyModel.py
@@ -8,15 +8,18 @@ class MyModel(object):
     """
     Model template. You can load your model parameters in __init__ from a location accessible at runtime
     """
-    fix = 1
-    model = None
-    def __init__(self, fix = 2, url = 'https://shield.mlamp.cn/task/api/file/space/download/bb000281867ad0a18fb40aaa2012d7b1/55766/sklearn_save.m'):
+    def __init__(self, fix = 2, url = 'https://shield.mlamp.cn/task/api/file/space/download/1098db05ad0bea9020e426327ff30580/56162/sklearn_save.m'):
         """
         Add any initialization parameters. These will be passed at runtime from the graph definition parameters defined in your seldondeployment kubernetes resource manifest.
         """
         print("Initializing")
         self.fix = fix
-        urllib.request.urlretrieve(url, "model.m")
+        self.url = url
+        self.loaded = False
+        self.model = None
+
+    def load(self):
+        urllib.request.urlretrieve(self.url, "model.m")
         self.model = joblib.load('model.m')
 
     def predict(self, X, features_names=None):
@@ -28,7 +31,9 @@ class MyModel(object):
         X : array-like
         feature_names : array of feature names (optional)
         """
-        print("Predict called - will run identity function")
+        if not self.loaded:
+            self.load()
+
         if self.model:
             return self.model.predict(X)
         else:
